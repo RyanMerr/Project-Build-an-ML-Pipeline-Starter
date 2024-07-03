c3435ca9063958fb5b1c74ad2d25d4b75a7d91d7
Ryan Merrithew <merrithewryan@gmail.com>
2024-07-02 19:38:42 -0600

    changing lat and long

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data, min_price, max_price):
+    min_price_in_data = data['price'].min()
+    max_price_in_data = data['price'].max()
+
+    assert data['price'].between(min_price, max_price).all() and (min_price <= min_price_in_data <= max_price) and (min_price <= max_price_in_data <= max_price)
